dc6404c469b689d9f26d69f4eba7d37aad38c02a
Mikhail Arkhipov <miklpostbox@gmail.com>
2019-11-14 13:47:12 +0300

    Remake all tests from String type to Integer type

--- src/test/java/innopolis/stc21/MA/MyGenericHashMapTest.java
+++ src/test/java/innopolis/stc21/MA/MyGenericHashMapTest.java
@@ -11,212 +11,212 @@ public class MyGenericHashMapTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void MyHashMapNegativeLoadFactor() {
-        MyGenericHashMap<String, String> myHashMap = new MyGenericHashMap<>(10, -0.1f);
+        MyGenericHashMap<Integer, Integer> myHashMap = new MyGenericHashMap<>(10, -0.1f);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void MyHashMapNegativeCapacity() {
-        MyGenericHashMap<String, String> myHashMap = new MyGenericHashMap<>(-10, 0.1f);
+        MyGenericHashMap<Integer, Integer> myHashMap = new MyGenericHashMap<>(-10, 0.1f);
     }
 
     @org.junit.Test
     public void size() {
-        MyGenericHashMap<String, String> myHashMapCustom = new MyGenericHashMap<>(100, 0.50f);
+        MyGenericHashMap<Integer, Integer> myHashMapCustom = new MyGenericHashMap<>(100, 0.50f);
         assertEquals(0, myHashMapCustom.size());
 
-        MyGenericHashMap<String, String> myHashMapDefault = new MyGenericHashMap<>();
+        MyGenericHashMap<Integer, Integer> myHashMapDefault = new MyGenericHashMap<>();
         assertEquals(0, myHashMapDefault.size());
-        for (int i = 0; i < 100; i++) {
-            myHashMapDefault.put(String.valueOf(i), String.valueOf(i));
+        for (Integer i = 0; i < 100; i++) {
+            myHashMapDefault.put(i, i);
         }
         assertEquals(100, myHashMapDefault.size());
     }
 
     @org.junit.Test
     public void isEmpty() {
-        MyGenericHashMap<String, String> myHashMap = new MyGenericHashMap<>();
+        MyGenericHashMap<Integer, Integer> myHashMap = new MyGenericHashMap<>();
         assertTrue(myHashMap.isEmpty());
-        myHashMap.put("test", "test");
+        myHashMap.put(0, 0);
         assertFalse(myHashMap.isEmpty());
     }
 
     @org.junit.Test
     public void containsKey() {
-        MyGenericHashMap<String, String> myHashMap = new MyGenericHashMap<>();
-        for (int i = 0; i < 200; i++) {
-            myHashMap.put(String.valueOf(i), String.valueOf(i));
+        MyGenericHashMap<Integer, Integer> myHashMap = new MyGenericHashMap<>();
+        for (Integer i = 0; i < 200; i++) {
+            myHashMap.put(i, i);
         }
-        for (int i = 0; i < 200; i++) {
-            assertTrue(myHashMap.containsKey(String.valueOf(i)));
+        for (Integer i = 0; i < 200; i++) {
+            assertTrue(myHashMap.containsKey(i));
         }
-        for (int i = 200; i < 210; i++) {
-            assertFalse(myHashMap.containsKey(String.valueOf(i)));
+        for (Integer i = 200; i < 210; i++) {
+            assertFalse(myHashMap.containsKey(i));
         }
     }
 
     @org.junit.Test
     public void containsValue() {
-        MyGenericHashMap<String, String> myHashMap = new MyGenericHashMap<>();
-        for (int i = 0; i < 200; i++) {
-            myHashMap.put(String.valueOf(i), String.valueOf(i));
+        MyGenericHashMap<Integer, Integer> myHashMap = new MyGenericHashMap<>();
+        for (Integer i = 0; i < 200; i++) {
+            myHashMap.put(i, i);
         }
-        for (int i = 0; i < 200; i++) {
-            assertTrue(myHashMap.containsValue(String.valueOf(i)));
+        for (Integer i = 0; i < 200; i++) {
+            assertTrue(myHashMap.containsValue(i));
         }
-        for (int i = 200; i < 210; i++) {
-            assertFalse(myHashMap.containsValue(String.valueOf(i)));
+        for (Integer i = 200; i < 210; i++) {
+            assertFalse(myHashMap.containsValue(i));
         }
     }
 
     @org.junit.Test
     public void get() {
-        MyGenericHashMap<String, String> myHashMap = new MyGenericHashMap<>();
-        for (int i = 0; i < 200; i++) {
-            myHashMap.put(String.valueOf(i), String.valueOf(i));
+        MyGenericHashMap<Integer, Integer> myHashMap = new MyGenericHashMap<>();
+        for (Integer i = 0; i < 200; i++) {
+            myHashMap.put(i, i);
         }
-        for (int i = 0; i < 200; i++) {
-            assertEquals(String.valueOf(i), myHashMap.get(String.valueOf(i)));
+        for (Integer i = 0; i < 200; i++) {
+            assertEquals(i, myHashMap.get(i));
         }
 
-        myHashMap.put(null, "NULL");
-        assertEquals("NULL", myHashMap.get(null));
+        myHashMap.put(null, 0);
+        assertEquals((Integer) 0, myHashMap.get(null));
 
-        myHashMap.put("null", null);
-        assertNull(myHashMap.get("null"));
+        myHashMap.put(201, null);
+        assertNull(myHashMap.get(201));
     }
 
     @org.junit.Test
     public void put() {
 
-        MyGenericHashMap<String, String> myHashMap = new MyGenericHashMap<>();
-        assertEquals("4", myHashMap.put("4", "4"));
+        MyGenericHashMap<Integer, Integer> myHashMap = new MyGenericHashMap<>();
+        assertEquals((Integer)4, myHashMap.put(4, 4));
 
-        assertEquals("12", myHashMap.put("12", "12"));
-        assertEquals("21", myHashMap.put("21", "21"));
+        assertEquals((Integer)12, myHashMap.put(12, 12));
+        assertEquals((Integer)21, myHashMap.put(21, 21));
 
-        assertEquals("12", myHashMap.put("12", "another"));
-        assertEquals("21", myHashMap.put("21", "another"));
+        assertEquals((Integer)12, myHashMap.put(12, 1000));
+        assertEquals((Integer)21, myHashMap.put(21, 1000));
 
-        assertEquals("123", myHashMap.put("123", "123"));
-        assertEquals("132", myHashMap.put("132", "132"));
-        assertEquals("213", myHashMap.put("213", "213"));
-        assertEquals("231", myHashMap.put("231", "231"));
-        assertEquals("312", myHashMap.put("312", "312"));
-        assertEquals("321", myHashMap.put("321", "321"));
+        assertEquals((Integer)123, myHashMap.put(123, 123));
+        assertEquals((Integer)132, myHashMap.put(132, 132));
+        assertEquals((Integer)213, myHashMap.put(213, 213));
+        assertEquals((Integer)231, myHashMap.put(231, 231));
+        assertEquals((Integer)312, myHashMap.put(312, 312));
+        assertEquals((Integer)321, myHashMap.put(321, 321));
 
-        assertEquals("123", myHashMap.put("123", "another"));
-        assertEquals("132", myHashMap.put("132", "another"));
-        assertEquals("213", myHashMap.put("213", "another"));
-        assertEquals("231", myHashMap.put("231", "another"));
-        assertEquals("312", myHashMap.put("312", "another"));
-        assertEquals("321", myHashMap.put("321", "another"));
+        assertEquals((Integer)123, myHashMap.put(123, 1000));
+        assertEquals((Integer)132, myHashMap.put(132, 1000));
+        assertEquals((Integer)213, myHashMap.put(213, 1000));
+        assertEquals((Integer)231, myHashMap.put(231, 1000));
+        assertEquals((Integer)312, myHashMap.put(312, 1000));
+        assertEquals((Integer)321, myHashMap.put(321, 1000));
 
         assertNull(myHashMap.put(null, null));
-        assertNull(myHashMap.put(null, "another"));
+        assertNull(myHashMap.put(null, 1000));
     }
 
     @org.junit.Test
     public void remove() {
-        MyGenericHashMap<String, String> myHashMap = new MyGenericHashMap<>();
-        for (int i = 0; i < 200; i++) {
-            myHashMap.put(String.valueOf(i), String.valueOf(i));
+        MyGenericHashMap<Integer, Integer> myHashMap = new MyGenericHashMap<>();
+        for (Integer i = 0; i < 200; i++) {
+            myHashMap.put(i, i);
         }
-        for (int i = 200; i < 210; i++) {
-            assertNull(myHashMap.remove(String.valueOf(i)));
+        for (Integer i = 200; i < 210; i++) {
+            assertNull(myHashMap.remove(i));
             assertEquals(200, myHashMap.size());
         }
-        for (int i = 0; i < 200; i++) {
-            assertEquals(String.valueOf(i), myHashMap.remove(String.valueOf(i)));
+        for (Integer i = 0; i < 200; i++) {
+            assertEquals(i, myHashMap.remove(i));
             assertEquals(200 - i - 1, myHashMap.size());
         }
     }
 
     @org.junit.Test
     public void putAll() {
-        MyGenericHashMap<String, String> myHashMap = new MyGenericHashMap<>();
-        for (int i = 0; i < 20; i++) {
-            myHashMap.put(String.valueOf(i), String.valueOf(i));
+        MyGenericHashMap<Integer, Integer> myHashMap = new MyGenericHashMap<>();
+        for (Integer i = 0; i < 20; i++) {
+            myHashMap.put(i, i);
         }
-        MyGenericHashMap<String, String> myHashMapAddon = new MyGenericHashMap<>();
-        for (int i = 20; i < 30; i++) {
-            myHashMapAddon.put(String.valueOf(i), String.valueOf(i));
+        MyGenericHashMap<Integer, Integer> myHashMapAddon = new MyGenericHashMap<>();
+        for (Integer i = 20; i < 30; i++) {
+            myHashMapAddon.put(i, i);
         }
 
         myHashMap.putAll(myHashMapAddon);
-        for (int i = 0; i < 30; i++) {
-            assertEquals(String.valueOf(i), myHashMap.get(String.valueOf(i)));
+        for (Integer i = 0; i < 30; i++) {
+            assertEquals(i, myHashMap.get(i));
         }
-        for (int i = 30; i < 40; i++) {
-            assertNull(myHashMap.get(String.valueOf(i)));
+        for (Integer i = 30; i < 40; i++) {
+            assertNull(myHashMap.get(i));
         }
         assertEquals(30, myHashMap.size());
 
-        MyGenericHashMap<String, String> myHashMapCreateByMap = new MyGenericHashMap<>(myHashMap);
-        for (int i = 0; i < 30; i++) {
-            assertEquals(String.valueOf(i), myHashMapCreateByMap.get(String.valueOf(i)));
+        MyGenericHashMap<Integer, Integer> myHashMapCreateByMap = new MyGenericHashMap<>(myHashMap);
+        for (Integer i = 0; i < 30; i++) {
+            assertEquals(i, myHashMapCreateByMap.get(i));
         }
-        for (int i = 30; i < 40; i++) {
-            assertNull(myHashMapCreateByMap.get(String.valueOf(i)));
+        for (Integer i = 30; i < 40; i++) {
+            assertNull(myHashMapCreateByMap.get(i));
         }
         assertEquals(30, myHashMapCreateByMap.size());
     }
 
     @org.junit.Test
     public void clear() {
-        MyGenericHashMap<String, String> myHashMap = new MyGenericHashMap<>();
-        myHashMap.put("4", "4");
-        myHashMap.put("12", "12");
-        myHashMap.put("21", "21");
+        MyGenericHashMap<Integer, Integer> myHashMap = new MyGenericHashMap<>();
+        myHashMap.put(4, 4);
+        myHashMap.put(12, 12);
+        myHashMap.put(21, 21);
         assertEquals(3, myHashMap.size());
 
         myHashMap.clear();
         assertEquals(0, myHashMap.size());
 
-        assertEquals("4", myHashMap.put("4", "4"));
-        assertEquals("12", myHashMap.put("12", "12"));
-        assertEquals("21", myHashMap.put("21", "21"));
+        assertEquals((Integer) 4, myHashMap.put(4, 4));
+        assertEquals((Integer)12, myHashMap.put(12, 12));
+        assertEquals((Integer)21, myHashMap.put(21, 21));
         assertEquals(3, myHashMap.size());
     }
 
     @org.junit.Test
     public void keySet() {
-        MyGenericHashMap<String, String> myHashMap = new MyGenericHashMap<>();
-        for (int i = 0; i < 20; i++) {
-            myHashMap.put(String.valueOf(i), String.valueOf(i));
+        MyGenericHashMap<Integer, Integer> myHashMap = new MyGenericHashMap<>();
+        for (Integer i = 0; i < 20; i++) {
+            myHashMap.put(i, i);
         }
-        Set<String> keys = myHashMap.keySet();
-        for (int i = 0; i < 20; i++) {
-            assertTrue(keys.contains(String.valueOf(i)));
+        Set<Integer> keys = myHashMap.keySet();
+        for (Integer i = 0; i < 20; i++) {
+            assertTrue(keys.contains(i));
             assertEquals(20, keys.size());
         }
-        for (int i = 20; i < 30; i++) {
-            assertFalse(keys.contains(String.valueOf(i)));
+        for (Integer i = 20; i < 30; i++) {
+            assertFalse(keys.contains(i));
         }
     }
 
     @org.junit.Test
     public void values() {
-        MyGenericHashMap<String, String> myHashMap = new MyGenericHashMap<>();
-        for (int i = 0; i < 20; i++) {
-            myHashMap.put(String.valueOf(i), String.valueOf(i));
+        MyGenericHashMap<Integer, Integer> myHashMap = new MyGenericHashMap<>();
+        for (Integer i = 0; i < 20; i++) {
+            myHashMap.put(i, i);
         }
-        Collection<String> values = myHashMap.values();
-        for (int i = 0; i < 20; i++) {
-            assertTrue(values.contains(String.valueOf(i)));
+        Collection<Integer> values = myHashMap.values();
+        for (Integer i = 0; i < 20; i++) {
+            assertTrue(values.contains(i));
             assertEquals(20, values.size());
         }
-        for (int i = 20; i < 30; i++) {
-            assertFalse(values.contains(String.valueOf(i)));
+        for (Integer i = 20; i < 30; i++) {
+            assertFalse(values.contains(i));
         }
     }
 
     @org.junit.Test
     public void entrySet() {
-        MyGenericHashMap<String, String> myHashMap = new MyGenericHashMap<>();
-        for (int i = 0; i < 200; i++) {
-            myHashMap.put(String.valueOf(i), String.valueOf(i));
+        MyGenericHashMap<Integer, Integer> myHashMap = new MyGenericHashMap<>();
+        for (Integer i = 0; i < 200; i++) {
+            myHashMap.put(i, i);
         }
-        Set<Map.Entry<String, String>> entries = myHashMap.entrySet();
+        Set<Map.Entry<Integer, Integer>> entries = myHashMap.entrySet();
         assertEquals(200, entries.size());
 
         class TestEntry<K, V> implements Map.Entry<K, V> {
@@ -272,8 +272,8 @@ public class MyGenericHashMapTest {
             }
         }
 
-        for (int i = 0; i < 200; i++) {
-            TestEntry<String, String> testEntry = new TestEntry<>(String.valueOf(i), String.valueOf(i));
+        for (Integer i = 0; i < 200; i++) {
+            TestEntry<Integer, Integer> testEntry = new TestEntry<>(i, i);
             assertTrue(entries.contains(testEntry));
         }
     }
